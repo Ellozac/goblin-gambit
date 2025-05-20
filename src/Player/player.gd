ce236a95c8ff65b8c77c5206a2a65157441643de
Ellozac <zlsy108@gmail.com>
2025-05-21 10:04:56 +1200

    Collision added

--- src/Player/player.gd
+++ src/Player/player.gd
@@ -1,31 +1,24 @@
 extends CharacterBody2D
-@export var speed: int = 125
-var PLAYER_POS = position
-# Called when the node enters the scene tree for the first time.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	PLAYER_POS = position
-	$AnimatedSprite2D.play()
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed('left'):
+@export var speed = 125
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+	# Flip sprite horizontally
+	if velocity.x < 0:
 		$AnimatedSprite2D.flip_h = true
-		velocity.x -= 1
-	if Input.is_action_pressed("right"):
+	elif velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false
-		velocity.x += 1
-	if Input.is_action_pressed('up'):
-		velocity.y -= 1
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-	if velocity.length() > 0:
-		$AnimatedSprite2D.animation = "Run"
-		
-		velocity = velocity.normalized() * speed
-	else:
-		rotation_degrees = 0
-		#$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.animation = "Idle"
 
-	position += velocity * delta
-	
+	# Play the correct animation
+	if velocity.length() == 0:
+		$AnimatedSprite2D.animation = "Idle"
+	else:
+		$AnimatedSprite2D.animation = "Run"
+
+	$AnimatedSprite2D.play()  # <- This is what was missing
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
